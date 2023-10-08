6356888d3d02f7c4b1db27fe16a4990db1c6f95c
Kolawole Moses <xoloyinks@gmail.com>
2023-10-07 20:12:20 -0700

    Updated

--- app/components/addExpense/page.js
+++ app/components/addExpense/page.js
@@ -12,60 +12,50 @@ import {
 } from "@/components/ui/card"
 
 
-export default function Kola(){
-  return(
+
+export default function NewExpenses({expenseName, expenseAmount, expenseCategory, handleExpenses, budgetArray}){
+  return (
     <>
-      <section>
-          Kolawole
-      </section>
+        <section>
+            <Card className="backdrop-blur-lg bg-blue-950/25">
+                <CardHeader>
+                    <CardTitle>Add New Expenses</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <form onSubmit={handleExpenses} action="" className='flex flex-col gap-5'> 
+
+                   <div className='flex flex-col gap-5 sm:flex-row sm:justify-between'>
+                     {/* Expenses name */}
+                     <div className='flex flex-col gap-3 sm:w-[49%]'>
+                      <Label htmlFor='expenses-name'>Expenses Name</Label>
+                      <Input ref={expenseName} placeholder='e.g. Coffee' type='text' id='expenses-name' />
+                    </div>
+
+                    {/* Expenses amount */}
+                    <div  className='flex flex-col gap-3 sm:w-[49%]'>
+                      <Label htmlFor='expenses-amount'>Amount</Label>
+                      <Input ref={expenseAmount} placeholder='-----' type='number' id='expenses-amount' />
+                    </div>
+                   </div>
+
+                    {/* Select Category */}
+                    <div  className='flex flex-col gap-3'>
+                      <Label htmlFor='budget-category'>Budget Category</Label>
+                        <select className='flex items-center justify-between w-full h-10 px-3 py-2 text-sm border rounded-md border-input bg-background ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50' ref={expenseCategory}>
+                              {
+                                budgetArray && budgetArray.map((datum, i) => <option key={i} value={datum.budgetName}>{datum.budgetName}</option> )
+                              }
+                        </select>
+                    </div>
+
+                    <Button className='w-fit dark:bg-green-700 dark:text-white'>
+                        Add Expenses
+                    </Button>
+                  </form>
+                </CardContent>
+            </Card>
+        </section>
     </>
-  ) 
+  )
 }
-
-
-// export default function NewExpenses({expenseName, expenseAmount, expenseCategory, handleExpenses, budgetArray}){
-//   return (
-//     <>
-//         <section>
-//             <Card className="backdrop-blur-lg bg-blue-950/25">
-//                 <CardHeader>
-//                     <CardTitle>Add New Expenses</CardTitle>
-//                 </CardHeader>
-//                 <CardContent>
-//                   <form onSubmit={handleExpenses} action="" className='flex flex-col gap-5'> 
-
-//                    <div className='flex flex-col gap-5 sm:flex-row sm:justify-between'>
-//                      {/* Expenses name */}
-//                      <div className='flex flex-col gap-3 sm:w-[49%]'>
-//                       <Label htmlFor='expenses-name'>Expenses Name</Label>
-//                       <Input ref={expenseName} placeholder='e.g. Coffee' type='text' id='expenses-name' />
-//                     </div>
-
-//                     {/* Expenses amount */}
-//                     <div  className='flex flex-col gap-3 sm:w-[49%]'>
-//                       <Label htmlFor='expenses-amount'>Amount</Label>
-//                       <Input ref={expenseAmount} placeholder='-----' type='number' id='expenses-amount' />
-//                     </div>
-//                    </div>
-
-//                     {/* Select Category */}
-//                     <div  className='flex flex-col gap-3'>
-//                       <Label htmlFor='budget-category'>Budget Category</Label>
-//                         <select className='flex items-center justify-between w-full h-10 px-3 py-2 text-sm border rounded-md border-input bg-background ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50' ref={expenseCategory}>
-//                               {
-//                                 budgetArray && budgetArray.map((datum, i) => <option key={i} value={datum.budgetName}>{datum.budgetName}</option> )
-//                               }
-//                         </select>
-//                     </div>
-
-//                     <Button className='w-fit dark:bg-green-700 dark:text-white'>
-//                         Add Expenses
-//                     </Button>
-//                   </form>
-//                 </CardContent>
-//             </Card>
-//         </section>
-//     </>
-//   )
-// }
 
